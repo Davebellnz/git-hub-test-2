c3dce58fa9e922f1da3145eaca2cf06815d4afb4
Davebellnz <davidbell.nz@gmail.com>
2016-02-02 12:09:48 +1300

    initial commit, starting project

---
+++ git_test.php
@@ -0,0 +1,21 @@
+<?php
+
+
+require_once("includes/header.php");
+require_once("includes/models/form.php");
+require_once("includes/models/view.php");
+require_once("includes/models/customer.php"); 
+
+
+?>
+
+<p>Yay!</p>
+<img class="underConstruction" src="assets/images/thumbs_up.jpg" alt="">
+
+
+
+<?php
+
+//header("Refresh: 1; URL=index.php");
+
+?>
