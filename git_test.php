7d5544a88da33707976f05b6da180500fbbb9d19
Davebellnz <davidbell.nz@gmail.com>
2016-02-02 12:17:12 +1300

    first / test update

--- git_test.php
+++ git_test.php
@@ -12,7 +12,7 @@ require_once("includes/models/customer.php");
 <p>Yay!</p>
 <img class="underConstruction" src="assets/images/thumbs_up.jpg" alt="">
 
-
+<div>Testing to see git changes 1</div>
 
 <?php
 
